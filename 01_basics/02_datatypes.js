026fd54eb53aad71eef85a577edc00134c2b036f
Abhinav-Rusia <abhinavrusia@gmail.com>
2024-08-21 06:30:58 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as Newer Version.
+
+// alert (3 + 3) we are using NodeJs not browser.
+
+console.log (3 + 3)
+
+console.log("Abhinav")
+
+
+let Name = "Abhinav" //string data type
+
+let age = 26 //number
+
+let isLoggedIn = false //boolean data type
+
+//  Number => 2 to power 53
+// bigint (used for very big number)
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value is not defined yet.
+// symbol => when we talk about something Unique.
+
+
+//Object
+
+console.log(typeof "abhinav");  // it will show string
+console.log(typeof age);  // it will show number
+console.log(typeof undefined);  // it will show undefined
+console.log(typeof null);  //it will show Object
